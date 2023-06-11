1693d1d391a8480c02aae323c95ab74670a0aa3d
kritishaaa <kritishamoktan530@gmail.com>
2023-06-11 21:58:03 +0545

    function delete

--- controllers/auth.js
+++ controllers/auth.js
@@ -68,20 +68,6 @@ const login = async (req, res) => {
     const accessToken = jwt.sign(User, process.env.ACCESS_TOKEN_SECRET);
     res.json({ accessToken: accessToken });
 
-    //function to Authenticate as a middleware
-    // function authenticateToken(req, res, next){
-    //   const authHeader =req.headers['authorization']
-    //   const token= authHeader && authHeader.split(' ')[1]
-    //   if (token ==null) return res.semdStatus(401)
-
-    //   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, User)=>{
-    //     if (err) return  res.sendStatus(403)
-    //     req.User =User
-    //     next();
-    //   })
-    
-    // }
-
     // Login successful
     return res.status(200).json({ message: 'Login successful' });
   }
