26924fa8fb4660afd300317d880f69bf085bb61c
Kritisha Moktan <92217803+kritishaaa@users.noreply.github.com>
2023-06-15 22:48:19 +0545

    updated

--- database/connection.js
+++ database/connection.js
@@ -6,11 +6,10 @@ const connectDb = (url) => {
    mongoose.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-
-
    }).then(() => {
       console.log("connection sucessfull");
-   }).catch((e) => {
+   }).catch((e) =>
+    {
       console.log("not connected");
       console.log(e);
    })
