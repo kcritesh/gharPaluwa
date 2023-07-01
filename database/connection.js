bb8e3a9d3a8ab95970cec1aa07697540d056b004
kritishaaa <kritishamoktan530@gmail.com>
2023-07-01 18:23:13 +0545

    merge conflicts

--- database/connection.js
+++ database/connection.js
@@ -2,13 +2,8 @@ import express from "express";
 import { connect } from "mongoose";
 
 const connectDb = (url) => {
-<<<<<<< HEAD
    console.log(url);
    connect(url, {
-=======
-   
-   mongoose.connect(url, {
->>>>>>> a78dfab939b6be568cf522e93648df3189f94530
       useNewUrlParser: true,
       useUnifiedTopology: true,
    }).then(() => {
